a609717f5c5f92bbf49747f82e007ee982e3165e
Agus9217 <justinibarrola92@gmail.com>
2024-08-07 14:38:05 -0300

    creando conexion con mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
 import { CatsMiddleware } from './cats/cats.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: ['.env']
+  }),
+  MongooseModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+      uri: configService.get<string>('MONGO_DB'),
+      connectionFactory(connection, name) {
+        console.log(connection.name)
+      },
+    }),
+    inject: [ConfigService],
+  }),
+],
 })
+
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
